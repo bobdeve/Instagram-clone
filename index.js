56bedad5626ff89fe6e7b3f894636d74d78844d7
bobdeve <122013187+bobdeve@users.noreply.github.com>
2023-04-10 20:12:35 +0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,72 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const profileImage1 = document.getElementById("img-pro");
+const profileImage2 = document.getElementById("img-pro1");
+const profileImage3 = document.getElementById("img-pro2");
+const imagePost1 = document.getElementById("image-posted");
+const imagePost2 = document.getElementById("image-posted1")
+const imagePost3 = document.getElementById("image-posted2");
+
+const mainContainer = document.querySelectorAll(".containermains")
+
+
+// const icons = document.querySelectorAll(".icons-img")
+
+
+profileImage1.setAttribute("src", posts[0].avatar);
+profileImage2.setAttribute("src", posts[1].avatar);
+profileImage3.setAttribute("src", posts[2].avatar);
+imagePost1.setAttribute("src", posts[0].post)
+imagePost2.setAttribute("src", posts[1].post)
+imagePost3.setAttribute("src",posts[2].post)
+
+for (let i = 0; i < mainContainer.length; i++) {
+   const container = mainContainer[i]
+   container.addEventListener("dblclick",function (){
+    //    alert("God is Good!");
+      var heartIcon = document.querySelectorAll(".icon-heart")
+      var likes = document.querySelectorAll(".likes")
+      console.log( heartIcon[i].src)
+      if (heartIcon[i].src === "file:///C:/Users/Bob/OneDrive/Desktop/Scrimba/Css%20fundamental%20Instagram%20clone/images/icon-heart.png"){
+        heartIcon[i].src= "images/red-icon.png"
+        var currentValue = parseInt(likes[i].textContent) + 1
+         likes[i].textContent=currentValue
+      }
+      else{
+        heartIcon[i].src= "images/icon-heart.png"
+        var currentValue = parseInt(likes[i].textContent) - 1
+        likes[i].textContent=currentValue
+      }
+    
+
+    })
+    
+}
